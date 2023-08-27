fb6391ce511a7c4f2d7c7bff92e78308911401e7
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2023-08-27 22:08:14 +0900

    add lifecycle rule

--- modules/site/s3.tf
+++ modules/site/s3.tf
@@ -27,5 +27,21 @@ data "aws_iam_policy_document" "static_website" {
       "${aws_s3_bucket.static_website.arn}/*"
     ]
   }
+}
 
+resource "aws_s3_bucket_lifecycle_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  rule {
+    # 約半年
+    id     = "181日経過で削除"
+    status = "Enabled"
+    expiration {
+      # 約半年
+      days = 180
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+  }
 }
